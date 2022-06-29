062bb6d12851c873624e3869e2e44c1138a96aae
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-06-29 13:46:11 -0300

    update initial repo

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        bai: ['Bai Jamjuree'],
+        open: ['Open Sans']
+      }
+    }
+  },
+  plugins: []
+}
