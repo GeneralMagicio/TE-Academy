f12f78fac7a5a6964189e5b6141ced4240a0b629
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-07-04 19:42:33 -0300

    feat: add navbar component

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Image from 'next/image'
+import classNames from 'classnames'
+import { Button } from '@/components/buttons/Button'
+
+interface INavbar {
+  isTransparent?: boolean
+}
+
+export function Navbar({ isTransparent }: INavbar) {
+  return (
+    <nav
+      className={classNames(
+        'absolute h-[100px] w-full px-20 py-5 flex justify-between items-center z-10',
+        isTransparent ? 'bg-transparent' : 'bg-white'
+      )}>
+      <div className="flex">
+        <Image height="52" src="/logos/TE/black.svg" width="52" />
+      </div>
+      <div className="flex gap-x-10 text-sm font-bold">
+        <div>Explore Courses</div>
+        <div>Curriculum</div>
+        <div>Events Calendar</div>
+      </div>
+      <div className="h-full flex gap-x-10 text-sm font-bold items-center">
+        <div>FAQ</div>
+        <span className="text-te-purple">Sign in</span>
+        <div className="h-14 w-48">
+          <Button label="Sign up" />
+        </div>
+      </div>
+    </nav>
+  )
+}
