ab20d9536ae1f396454e357111a28c76695adde4
Pedro Kretzschmar <pedro.kretz@gmail.com>
2022-07-08 11:49:37 -0300

    hotfix: remove imakai as image loader

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  images: {
-    loader: 'akamai',
-    path: ''
-  },
   reactStrictMode: true,
   trailingSlash: true,
   webpack: (cfg) => {
